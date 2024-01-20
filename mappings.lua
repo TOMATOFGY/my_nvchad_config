8238c90cbcc267b7c9fdc2ccd47db95b9a7e47b5
TOMATOFGY <tomatofaq@gmail.com>
2024-01-20 19:54:12 +0800

    Basic mappings

--- mappings.lua
+++ mappings.lua
@@ -3,14 +3,20 @@ local M = {}
 
 M.general = {
   n = {
+    ["<leader>w"] = { ":w <CR>", "save quickly" , opts = { nowait = true } },
+    ["<leader>s"] = { "<cmd> w <CR>", "save quickly" , opts = { nowait = true } },
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-
     --  format with conform
     ["<leader>fm"] = {
       function()
         require("conform").format()
       end,
       "formatting",
+    },
+
+    ["<A-e>"] = {
+       "<cmd> NvimTreeFocus <CR>" ,
+  "Also Focus nvimtree"
     }
 
   },
@@ -20,5 +26,12 @@ M.general = {
 }
 
 -- more keybinds!
+-- use alt-num to do quick switch
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
+
 
 return M
